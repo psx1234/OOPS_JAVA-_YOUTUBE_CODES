961d3d602a1c13852a5ab6ede23c4564ee52fcf8
psx1234 <144951464+psx1234@users.noreply.github.com>
2023-10-04 20:00:00 +0530

    Add files via upload

---
+++ Oops24.java
@@ -0,0 +1,35 @@
+package oops5;
+
+class C1 {
+    public int x = 5;
+    protected int y = 45;
+    int z = 6;
+    private int a = 78;
+
+    public void seta(int a) {
+        this.a = a;
+    }
+
+    public int geta() {
+        return a;
+    }
+
+    public void meth1() {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
+
+public class Oops24 {
+
+    public static void main(String[] args) {
+        C1 c = new C1();
+        // c.meth1();
+        System.out.println(c.x);
+        System.out.println(c.y);
+        System.out.println(c.z);
+        System.out.println(c.geta());
+    }
+}
